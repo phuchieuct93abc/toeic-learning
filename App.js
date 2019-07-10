8df153ae988c2ef974b9bf6aab2964bc920764d6
hieu <phuchieuct93@gmail.com>
2019-07-10 21:40:46 +0700

    Initialize

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  let blog = "https://frontend.cloudaccess.host/";
+
+  let logo = "https://frontend.cloudaccess.host/wp-content/uploads/2019/03/010a9061-dcf4-4dd9-b879-ba1a7305280f-150x150.png"
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}  alt="logo" />
+        Welcome to        <a href="{blog}">Frontend        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
