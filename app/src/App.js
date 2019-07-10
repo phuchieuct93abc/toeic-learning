bf048d69dbb0fbcf5bab21f0846ce6a9db8a12d9
hieu <phuchieuct93@gmail.com>
2019-07-10 21:41:08 +0700

    Update

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let blog = "https://frontend.cloudaccess.host/";
+
+  let logo = "https://frontend.cloudaccess.host/wp-content/uploads/2019/03/010a9061-dcf4-4dd9-b879-ba1a7305280f-150x150.png"
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo}  alt="logo" />
+        Welcome to        <a href="{blog}">Frontend        </a>
       </header>
     </div>
   );
