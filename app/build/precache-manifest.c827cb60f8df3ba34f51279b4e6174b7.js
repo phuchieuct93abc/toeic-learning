a7774d869bce07d6f5e22e885c2127a0dab5b979
hieu <phuchieuct93@gmail.com>
2019-07-14 11:00:07 +0700

    Update CD/CI

---
+++ app/build/precache-manifest.c827cb60f8df3ba34f51279b4e6174b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a54c0622e65f5b9fa7abb4ecbde99f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de0c0d00bdb8d465b21e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "de0c0d00bdb8d465b21e",
+    "url": "/static/js/main.652c1450.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  let blog = "https://frontend.cloudaccess.host/";
 
   let logo = "https://frontend.cloudaccess.host/wp-content/uploads/2019/03/010a9061-dcf4-4dd9-b879-ba1a7305280f-150x150.png"
   return (
@@ -10,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo}  alt="logo" />
         Welcome to        <a href="{blog}">Frontend        </a>
+        Check to see if CircleCI works
       </header>
     </div>
   );
