6d02c35a1f95be337e0d90cd85144471e5a324c4
hieu <phuchieuct93@gmail.com>
2019-07-15 21:54:10 +0700

    Refactor

---
+++ app/src/Dashboard.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import {Card} from 'antd';
+
+export interface TestData {
+    name: string,
+    description: string,
+    id: number
+}
+
+export default class Dashboard extends React.Component {
+
+    // @ts-ignore
+    private tests: TestData[] = [
+        {
+            description: "Desc 1",
+            id: 1,
+            name: "Name 1"
+
+        },
+        {
+            description: "Desc 2",
+            id: 2,
+            name: "Name 2"
+
+        }, ,
+        {
+            description: "Desc 3",
+            id: 3,
+            name: "Name 3"
+
+        },
+        {
+            description: "Desc 4",
+            id: 4,
+            name: "Name 4"
+
+        },
+        {
+            description: "Desc 5",
+            id: 5,
+            name: "Name 5"
+
+        }];
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        return (
+            <div style={{background: '#ECECEC', padding: '30px'}}>
+                {this.tests.map(t => {
+
+                    return (
+                        <Card title={t.name} key={t.id} style={{width: 300}}>
+                            {t.description}
+                        </Card>
+                    )
+                })}
+
+
+            </div>
+        )
+    }
+}
