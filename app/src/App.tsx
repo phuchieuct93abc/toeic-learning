0a898b1d9e0b8a808f6b7e3977b92ae7e3c6d1b4
hieu <phuchieuct93@gmail.com>
2019-07-15 21:54:10 +0700

    Refactor

---
+++ app/src/App.tsx
@@ -0,0 +1,54 @@
+import { Breadcrumb, Icon, Layout, Menu } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import React from 'react';
+import './App.css';
+import AppSider from "./AppSider";
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+export default class App extends React.Component<{}, {}> {
+  constructor(props: any) {
+    super(props);
+  }
+  render() {
+    return (
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <AppSider/>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              style={{
+                background: '#fff',
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              Content
+          </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    )
+  }
+
+
+} 
