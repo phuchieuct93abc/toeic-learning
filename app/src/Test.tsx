14f36fc4417be042ac1c4673a5141082ba65e925
hieu <phuchieuct93@gmail.com>
2019-07-20 11:39:52 +0700

    Update test data

--- app/src/Test.tsx
+++ app/src/Test.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
 import TestService from "./services/TestService";
-import {Button} from "antd";
+import {Button, Radio} from "antd";
 import {TestModel} from "./models/TestModel";
 import Popover from "antd/lib/popover";
+import "./test.css"
+import Icon from "antd/lib/icon";
 
 export default class Test extends React.Component<{ match: any }, { testId: number, testData?: TestModel, mp3?: string }> {
 
@@ -52,7 +54,7 @@ export default class Test extends React.Component<{ match: any }, { testId: numb
 
     renderMp3() {
         if (this.state.mp3) {
-            return (<audio controls autoPlay>
+            return (<audio controls autoPlay style={{width: "100%"}}>
                 <source src={this.state.mp3} type="audio/mpeg"></source>
             </audio>)
         }
@@ -64,38 +66,63 @@ export default class Test extends React.Component<{ match: any }, { testId: numb
         }
     }
 
+    renderAnswer() {
+        if (!this.state.testData) return;
+        return (<div>
+            <Radio.Group size="large">
+                {this.state.testData.questions[0].options.map((q, index) => {
+                    return (<Radio style={{display: "block"}} value={index}>{["A", "B", "C", "D"][index]}. {q}</Radio>
+                    )
+                })}
+
+            </Radio.Group>
+
+        </div>)
+    }
+
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
 
         if (this.state.testData) {
             return (
 
                 <div style={{overflow: "auto", maxHeight: "100%"}}>
-                    <div>{this.renderImage()}</div>
+                    <div className={"test-wrapper"}>
+                        <div>
+                            {this.renderImage()}
+                            {this.renderMp3()}
 
-                    {this.renderMp3()}
+                        </div>
 
 
-                    <div>
+                        <div>
 
-                        <Popover content={this.hint()} title="Hint" trigger="click">
-                            <Button>Hover me</Button>
-                        </Popover>
+                            {this.renderAnswer()}
+                            <Popover content={this.hint()} title="Hint" trigger="click">
+                                <Button>Hover me</Button>
+                            </Popover>
+
+                        </div>
+
+
+                        <Button.Group size="large" className={"navigator"}>
+
+
+                            <Link to={(this.state.testId - 1).toString()}>
+                                <Button type="primary">
+                                    <Icon type="left"/>
+                                    Backward
+                                </Button> </Link>
+
+                            <Link to={(this.state.testId + 1).toString()} style={{float: "right"}}>
+                                <Button type="primary">
+                                    Forward
+                                    <Icon type="right"/>
+                                </Button>
+                            </Link>
+                        </Button.Group>
+
 
                     </div>
-
-
-                    <div className={"navigator"}>
-                        <Link to={(this.state.testId - 1).toString()}>
-                            <Button type="default">Back</Button>
-                        </Link>
-
-                        <Link to={(this.state.testId + 1).toString()}>
-                            <Button
-                                type="primary">Next </Button>
-                        </Link>
-
-                    </div>
-
                 </div>
 
             )
