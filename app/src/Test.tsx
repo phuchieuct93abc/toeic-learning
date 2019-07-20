dfaa7609b668bb5603eb557073756fcbdc74d245
hieu <phuchieuct93@gmail.com>
2019-07-20 15:28:19 +0700

    Update cache data

--- app/src/Test.tsx
+++ app/src/Test.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
 import TestService from "./services/TestService";
-import {Button, Radio} from "antd";
-import {TestModel} from "./models/TestModel";
+import { Button, Radio } from "antd";
+import { TestModel } from "./models/TestModel";
 import Popover from "antd/lib/popover";
 import "./test.css"
-import Icon from "antd/lib/icon";
+import Navigator from './Navigator';
 
 export default class Test extends React.Component<{ match: any }, { testId: number, testData?: TestModel, mp3?: string }> {
 
@@ -21,18 +20,19 @@ export default class Test extends React.Component<{ match: any }, { testId: numb
 
     }
 
+
     initData() {
         let testGroup = this.props.match.params.testGroup;
         let testId = this.props.match.params.testId;
         this.testService.getTestsById(testGroup, testId).then(data => {
 
-            this.setState({testData: data, mp3: this.testService.getMp3(testGroup, testId)})
+            this.setState({ testData: data, mp3: this.testService.getMp3(testGroup, testId) })
         });
     }
 
     componentDidMount(): void {
         // @ts-ignore
-        this.setState({testId: parseInt(this.props.match.params.testId), testData: null});
+        this.setState({ testId: parseInt(this.props.match.params.testId), testData: null });
         this.initData();
 
 
@@ -41,28 +41,28 @@ export default class Test extends React.Component<{ match: any }, { testId: numb
     componentWillReceiveProps(nextProps: Readonly<{ match: any }>, nextContext: any): void {
 
         // @ts-ignore
-        this.setState({testId: parseInt(nextProps.match.params.testId), testData: null});
+        this.setState({ testId: parseInt(nextProps.match.params.testId), testData: null });
         this.initData();
     }
 
     hint() {
         if (this.state.testData) {
-            let htmlData = {__html: this.state.testData.hint};
-            return (<div dangerouslySetInnerHTML={htmlData}/>)
+            let htmlData = { __html: this.state.testData.hint };
+            return (<div dangerouslySetInnerHTML={htmlData} />)
         }
     }
 
     renderMp3() {
         if (this.state.mp3) {
-            return (<audio controls autoPlay style={{width: "100%"}}>
-                <source src={this.state.mp3} type="audio/mpeg"/>
+            return (<audio controls autoPlay style={{ width: "100%" }}>
+                <source src={this.state.mp3} type="audio/mpeg" />
             </audio>)
         }
     }
 
     renderImage() {
         if (this.state.testData && this.state.testData.image) {
-            return (<img src={this.testService.getImage(this.state.testData.image)} alt={this.state.testData.image}/>)
+            return (<img src={this.testService.getImage(this.state.testData.image)} alt={this.state.testData.image} />)
         }
     }
 
@@ -71,7 +71,7 @@ export default class Test extends React.Component<{ match: any }, { testId: numb
         return (<div>
             <Radio.Group size="large">
                 {this.state.testData.questions[0].options.map((q, index) => {
-                    return (<Radio style={{display: "block"}} value={index}>{["A", "B", "C", "D"][index]}. {q}</Radio>
+                    return (<Radio style={{ display: "block" }} value={index}>{["A", "B", "C", "D"][index]}. {q}</Radio>
                     )
                 })}
 
@@ -85,7 +85,7 @@ export default class Test extends React.Component<{ match: any }, { testId: numb
         if (this.state.testData) {
             return (
 
-                <div style={{overflow: "auto", maxHeight: "100%"}}>
+                <div style={{ overflow: "auto", maxHeight: "100%" }}>
                     <div className={"test-wrapper"}>
                         <div>
                             {this.renderImage()}
@@ -103,23 +103,9 @@ export default class Test extends React.Component<{ match: any }, { testId: numb
 
                         </div>
 
+                        <Navigator prevId={this.state.testId - 1} nextId={this.state.testId + 1} />
 
-                        <Button.Group size="large" className={"navigator"}>
 
-
-                            <Link to={(this.state.testId - 1).toString()}>
-                                <Button type="primary">
-                                    <Icon type="left"/>
-                                    Backward
-                                </Button> </Link>
-
-                            <Link to={(this.state.testId + 1).toString()} style={{float: "right"}}>
-                                <Button type="primary">
-                                    Forward
-                                    <Icon type="right"/>
-                                </Button>
-                            </Link>
-                        </Button.Group>
 
 
                     </div>
